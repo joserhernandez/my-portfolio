33096c8f2b877d6eeded6461e27fb622fbda0073
joserhernandez <00366718@uca.edu.sv>
2024-01-22 22:33:25 -0600

    Update: Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,32 +15,31 @@ const Home = () => {
     return (
         <>
             <div className='vh-100 card-overlay'>
-            <Container fluid className='vh-100 bg-image d-flex flex-column'>
-                
-                <Row><NavBar/></Row>
-                <Row className='flex-grow-1'>
-                    {/* <Col className='d-flex justify-content-center align-items-center'>
-                        <Image src="src/assets/image1.svg" rounded />
-                    </Col> */}
-                    <Col className='d-flex flex-column justify-content-center align-items-center'>
-                        <div>
-                            <h1 className='font-primary'>José R. Hernández</h1>
+                <Container fluid className='vh-100 bg-image d-flex flex-column'
+                    >
+
+                    <Row><NavBar /></Row>
+                    <Row className='flex-grow-1 p-5'>
+                        <div className='d-flex flex-column justify-content-center'>
+                            <div className='p-2'>
+                                <h1 className='font-primary' style={{fontSize: '5rem', whiteSpace: 'nowrap'}}>José R. <br/> Hernández</h1>
+                            </div>
+                            <div className="p-2">
+                                <p className='font-secondary fs-4' style={{ color: '#F8E559', textTransform: 'uppercase', fontWeight: '600'}}>Frontend Developer</p>
+                            </div>
+                            {/* <div className="mt-3 p-2">
+                                <p className='fs-5 font-secondary'>"Excellence is not a destination, it is a continuous journey of improvement."</p>
+                            </div> */}
                         </div>
-                        <div className="p-2">
-                            <p className='font-secondary fs-3' style={{color: '#F8E559', textTransform: 'uppercase', fontWeight: '600'}}>Frontend Developer</p>
-                        </div>
-                        <div className="mt-3 p-2">
-                            <p className='text-center fs-5 font-secondary'>"Excellence is not a destination, it is a continuous journey of improvement."</p>
-                        </div>
-                    </Col>
-                </Row>
-                <a href="http://www.freepik.com" 
-                    className='text-light text-decoration-none pb-1' 
-                    style={{fontSize: '12px', opacity: '25%', fontWeight: '200'}}>
-                    Designed by fullvector / Freepik
-                </a>
-                
-            </Container></div>
+                    </Row>
+                    <a href="http://www.freepik.com"
+                        className='text-light text-decoration-none pb-1'
+                        style={{ fontSize: '12px', opacity: '25%', fontWeight: '200' }}>
+                        Designed by fullvector / Freepik
+                    </a>
+
+                </Container>
+            </div>
 
         </>
     )
